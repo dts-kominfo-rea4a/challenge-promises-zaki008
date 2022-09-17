f4d2613765852f75f3889ed665cdca12bd049cd6
zakiteravintech08 <a.yamani@teravintech.com>
2022-09-17 09:31:20 +0700

    count the number of angry and not angry

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+// const promiseOutput = null;
+
+const promiseOutput = async (emosi) => {
+  try {
+    const dataTeaterIXX = await promiseTheaterIXX();
+    const dataTeaterVGC = await promiseTheaterVGC();
+
+    const dataVGC = dataTeaterVGC.filter((res) => res.hasil === emosi);
+    const dataIXX = dataTeaterIXX.filter((res) => res.hasil === emosi);
+    return dataVGC.length + dataIXX.length;
+  } catch (error) {}
+};
 
 module.exports = {
   promiseOutput,
